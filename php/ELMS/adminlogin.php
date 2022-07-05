c474d5d528c7abec687e70ed9c8f6d837a930f09
karthick1016 <94736866+karthick1016@users.noreply.github.com>
2022-07-05 15:53:52 +0530

    Add files via upload

---
+++ php/ELMS/adminlogin.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Admin</title>
+	<link rel="stylesheet" type="text/css" href="styles.css">
+</head>
+<body>
+	<div class="container">
+		<form>
+			 <div class="login_head">
+			 	<h4>Admin Panel</h4>
+			 	<p>Employee Leave Management System</p>
+			 </div>
+			 <div class="login_body">
+			 	<div class="elements">
+			 		<label for="uname">Username:</label>
+			 		<input type="text" id="uname" name="username">
+			 	</div>
+			 	<div class="elements">
+			 		<label for="password">Password</label>
+			 		<input type="password" id="password" name="password">
+			 	</div>
+			 	<div class="btn">
+			 		<button>Submit</button>
+			 	</div>
+			 	<div class="admin_panel">
+			 		<p><- GoBack</p>
+			 	</div>
+			 </div>
+		</form>
+	</div>
+</body>
+</html>
---
+++ php/ELMS/index.php
@@ -0,0 +1,40 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" type="text/css" href="styles.css">
+	<title>Employee Leave Management System</title>
+</head>
+<body>
+	<div class="container">	
+		<form>
+			<div class="login_head">
+		 		<h4>Employee Login Panel</h4>
+		 		<p>Employee Leave Management System</p>
+			</div>
+			<div class="login_body">
+				<div class="elements">
+				<label for="emp_email">Email Address</label>
+				<input type="email" id="emp_email" name="email" required>
+				</div>
+				<div class="elements">
+				<label for="emp_password">Password</label>
+				<input type="password" id="emp_password" name="password" required>
+				</div>
+				<div class="forgot_password">
+				<p>Forgot Password</p>
+				</div>
+				<div class="btn">
+				<button>Login -></button>
+				</div>
+				<div class="admin_panel">
+				<p>Go to Admin Panel</p>
+				</div>
+			</div>
+			
+		</form>
+	</div>
+</body>
+</html>
