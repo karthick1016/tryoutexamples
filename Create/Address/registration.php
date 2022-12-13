617eb600f659f8cb391f6550e6b15191883d65b5
karthick1016 <94736866+karthick1016@users.noreply.github.com>
2022-12-13 19:20:35 +0530

    Delete Create/Address directory

--- Create/Address/registration.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-use Magento\Framework\Component\ComponentRegistrar;
-
-ComponentRegistrar::register(
-    ComponentRegistrar::MODULE,
-    'Create_Address',
-    __DIR__
-);
--- Create/Address/view/frontend/requirejs-config.js
+++
@@ -1,7 +0,0 @@
-var config = {
-    "map": {
-        "*": {
-            'Magento_Checkout/js/model/shipping-save-processor/default': 'Create_Address/js/model/shipping-save-processor/default'
-        }
-    }
-};
